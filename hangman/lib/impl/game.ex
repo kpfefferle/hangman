14bbbc8b27c94f476a7dd229f02248856378a4f4
Kevin Pfefferle <kevin@pfefferle.me>
2022-05-13 16:24:00 -0400

    Cleanup

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -15,7 +15,7 @@ defmodule Hangman.Impl.Game do
     used:       MapSet.new()
   )
 
-  ####################################
+  ### new_game ###
 
   @spec new_game :: t
   def new_game do
@@ -29,7 +29,7 @@ defmodule Hangman.Impl.Game do
     }
   end
 
-  ####################################
+  #### make_move ###
 
   @spec make_move(t, String.t) :: { t, Type.tally }
   def make_move(game = %{ game_state: state }, _guess)
