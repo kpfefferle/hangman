ce8343fa4f900eb93b5dec219df4bda3d2da0ae7
Kevin Pfefferle <kevin@pfefferle.me>
2022-05-13 20:34:57 -0400

    Handle guesses

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -4,7 +4,6 @@ defmodule HangmanImplGameTest do
 
   test "new game returns structure" do
     game = Game.new_game
-
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
@@ -12,7 +11,6 @@ defmodule HangmanImplGameTest do
 
   test "new game returns correct work" do
     game = Game.new_game("wombat")
-
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert game.letters === ["w", "o", "m", "b", "a", "t"]
@@ -23,8 +21,25 @@ defmodule HangmanImplGameTest do
       game = Game.new_game("wombat")
       game = Map.put(game, :game_state, state)
       { new_game, _tally } = Game.make_move(game, "x")
-
       assert new_game == game
     end
+  end
+
+  test "a duplicate letter is reported" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "we record letters used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "y")
+    {game, _tally} = Game.make_move(game, "x")
+    assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
   end
 end
