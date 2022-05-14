281b80290a8d7fb6d4a9cc9a02ca5f73951ee5d3
Kevin Pfefferle <kevin@pfefferle.me>
2022-05-14 15:59:07 -0400

    Cleanup

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -24,9 +24,8 @@ defmodule TextClient.Impl.Player do
   def interact({ game, tally }) do
     IO.puts feedback_for(tally)
     IO.puts current_word(tally)
-    guess = get_guess()
-    { updated_game, updated_tally } = Hangman.make_move(game, guess)
-    interact({ updated_game, updated_tally })
+    Hangman.make_move(game, get_guess())
+    |> interact()
   end
 
   def feedback_for(tally = %{ game_state: :initializing }) do
