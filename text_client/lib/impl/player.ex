fe8eab4cf40f2b2c4eee7fc537ed6595593f34e7
Kevin Pfefferle <kevin@pfefferle.me>
2022-05-14 15:55:06 -0400

    Working text_client

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -13,23 +13,21 @@ defmodule TextClient.Impl.Player do
 
   @spec interact(state) :: :ok
 
-  def interact({_game, _tally = %{ game_state: :won }}) do
-    IO.puts "Congratulations, you won!"
+  def interact({game, _tally = %{ game_state: :won }}) do
+    IO.puts "Congratulations, you won! The word was #{game_answer(game)}"
   end
 
-  def interact({_game, tally = %{ game_state: :lost }}) do
-    IO.puts "Sorry, you lost... the work was #{tally.letters |> Enum.join}"
+  def interact({game, %{ game_state: :lost }}) do
+    IO.puts "Sorry, you lost... the word was #{game_answer(game)}"
   end
 
-  def interact({ _game, tally }) do
+  def interact({ game, tally }) do
     IO.puts feedback_for(tally)
-    # display current word
-    # get next guess
-    # make move
-    # interact()
+    IO.puts current_word(tally)
+    guess = get_guess()
+    { updated_game, updated_tally } = Hangman.make_move(game, guess)
+    interact({ updated_game, updated_tally })
   end
-
-  # @type state :: :initializing | :good_guess | :bad_guess | :already_used
 
   def feedback_for(tally = %{ game_state: :initializing }) do
     "Welcome! I'm thinking of a #{tally.letters |> length} letter word"
@@ -38,4 +36,25 @@ defmodule TextClient.Impl.Player do
   def feedback_for(%{ game_state: :good_guess }),   do: "Good guess!"
   def feedback_for(%{ game_state: :bad_guess }),    do: "Sorry, that letter's not in the word"
   def feedback_for(%{ game_state: :already_used }), do: "You already used that letter"
+
+  def current_word(tally) do
+    [
+      "Word so far: ",
+      tally.letters |> Enum.join(" "),
+      "   turns left: ",
+      tally.turns_left |> to_string,
+      "   used so far: ",
+      tally.used |> Enum.join(","),
+    ]
+  end
+
+  def get_guess() do
+    IO.gets("Next letter: ")
+    |> String.trim()
+    |> String.downcase()
+  end
+
+  defp game_answer(game) do
+    game.letters |> Enum.join
+  end
 end
